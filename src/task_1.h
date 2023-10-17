0560ee4f6ea9a821f951e3c07d3f25fdd32b10d7
Jamol <127374162+Jamol8892@users.noreply.github.com>
2023-10-17 22:03:52 +0500

    Update task_1.h

--- src/task_1.h
+++ src/task_1.h
@@ -1,5 +1,53 @@
 /*
- * Author:
- * Date:
+ * Author: Jamol
+ * Date: 17.10.2023
  * Name:
  */
+#include <string>
+
+class FamilyMembers {
+protected:
+    std::string name;
+    int age;
+
+public:
+    FamilyMembers(std::string name, int a) : name(name), age(a) {}
+    virtual void display() {
+        std::cout << "Name: " << name << ", Age: " << age << std::endl;
+    }
+};
+
+class Brothers : public FamilyMembers {
+private:
+    std::string Occupation;
+
+public:
+    Brothers(std::string name, int a, std::string o) : FamilyMembers(name, a), Occupation(o){};
+    void display() override{
+        std::cout << "Nephew's name: " << name << ", Age: " << age << ", Kindergarten: " << Occupation << std::endl;
+
+    }
+};
+
+class Nephews : public FamilyMembers {
+private:
+    std::string kinderGartenName;
+
+public:
+    Nephews(std::string name, int a, std::string k) : FamilyMembers(name, a), kinderGartenName(k){};
+    void display() override{
+        std::cout << "Nephew's name: " << name << ", Age: " << age << ", Kindergarten: " << kinderGartenName << std::endl;
+
+    }
+};
+
+class Parents : public FamilyMembers{
+private:
+    std::string occupation;
+
+public:
+    Parents(std::string name, int a, std::string o) : FamilyMembers(name, a), occupation(o) {}
+    void display() override {
+        std::cout << "Parents' name: " << name << ", Age: " << age << ", Occupation: " << occupation << std::endl;
+    }
+};
